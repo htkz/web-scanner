d5c11e43617aa8276a13abf19245446e1e4cc608
htkz <htkzmo@gmail.com>
2017-12-10 07:06:56 -0500

    Added fake data on structure

--- tmp/js/trans.js
+++ tmp/js/trans.js
@@ -2,10 +2,12 @@ transToSearch = () => {
     $('#result').hide('400', () => {
         $('.search-wraper').show('400');
     });
+    $('.backToSearch').hide();
 }
 
 transToResult = () => {
     $('.search-wraper').hide('400', () => {
         $('#result').show('400');
     });
+    $('.backToSearch').show();
 }
